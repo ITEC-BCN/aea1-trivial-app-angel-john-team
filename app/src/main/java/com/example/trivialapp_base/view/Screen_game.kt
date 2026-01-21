ecc1526a72637e4d21c5dd00f8d2b741eb5f7eb7
angel-abraham.flores.7e8 <angel-abraham.flores.7e8@itb.cat>
2026-01-21 09:49:07 +0100

    Fix, Finifsh in class. Continue in home

--- app/src/main/java/com/example/trivialapp_base/view/Screen_game.kt
+++ app/src/main/java/com/example/trivialapp_base/view/Screen_game.kt
@@ -1,12 +1,15 @@
 package com.example.trivialapp_base.view
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import com.example.trivialapp_base.viewmodel.GameViewModel
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Text
 
 @Composable
 fun GameScreen(navController: NavController, viewModel: GameViewModel) {
@@ -15,6 +18,11 @@ fun GameScreen(navController: NavController, viewModel: GameViewModel) {
         modifier = Modifier
             .fillMaxSize()
     ) {
+        Column() {
+            //Text( text = viewModel.preguntaActual)
+            Row() {
 
+            }
+        }
     }
 }
