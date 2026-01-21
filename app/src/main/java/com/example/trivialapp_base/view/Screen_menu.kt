bb64553634d3d7efe246233a0f02a518fa74c0f3
johndevera7e7 <john.devera.7e7@itb.cat>
2026-01-21 09:08:38 +0100

    Feat: Creating game menu (Created and placed title, logo, buttons)

--- app/src/main/java/com/example/trivialapp_base/view/Screen_menu.kt
+++ app/src/main/java/com/example/trivialapp_base/view/Screen_menu.kt
@@ -1,31 +1,83 @@
 package com.example.trivialapp_base.view
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.layout
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.max
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.NavHost
+import androidx.navigation.R
 import com.example.trivialapp_base.viewmodel.GameViewModel
-
 
 
 @Composable
 fun MenuScreen(navController: NavController, viewModel: GameViewModel) {
-    Column(
+    var selectedDifficulty: String by remember {mutableStateOf("")}
+    var expanded: Boolean by remember { mutableStateOf(false) }
+    val difficulties = listOf("Easy", "Medium", "Hard")
+
+    Box(
+        contentAlignment = Alignment.Center,
         modifier = Modifier
             .fillMaxSize()
     ) {
-        Text(
-            text = "Awesome App",
-            fontSize = TextUnit.Unspecified
+        Column(
+        ) {
+            Image(
+                painter = painterResource(id = com.example.trivialapp_base.R.drawable.logotrivial),
+                contentDescription = "Logo trivial",
+                alpha = 1f
+            )
+            Text(
+                text = "Awesome App",
+                fontSize = 32.sp
+            )
+            DropdownMenu(
+                expanded = expanded,
+                onDismissRequest = {expanded = false},
+                modifier = Modifier
+                    .width(200.dp)
+            ) {
+                difficulties.forEach { difficulties ->
+                    DropdownMenuItem(
+                        text = {Text(text = difficulties)},
+                        onClick = {
+                            expanded = false
+                            selectedDifficulty = difficulties
+                        }
+                    )
+                }
+            }
+            Button(
+                onClick = { navController.navigate("GameScreen") },
+                modifier = Modifier
+                    .width(200.dp)
+            ) {
+                Text(text = "New Game")
+            }
+        }
 
-        )
     }
 }
